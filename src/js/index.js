cdd30adccb4e2e41e46296c2f6ea67a06e34f286
AleksandrNebesnyi <a.nebesnyi@gmail.com>
2024-03-29 09:38:59 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import Notiflix from 'notiflix';
+import axios from 'axios'
+// axios.defaults.baseURL = 'https://api.example.com';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+// Notiflix.Notify.info('Cogito ergo sum');
+
+// Notiflix.Notify.success(
+//   'Click Me',
+//   function cb() {
+//     // callback
+//   },
+// );
